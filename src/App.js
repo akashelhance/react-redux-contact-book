c220cb22a46fd979e4316c3e6e7b932db63f9a29
akash elhance <akashelhance@gmail.com>
2021-04-14 13:01:26 +0530

    dummy table setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import Navbar from "./components/elements/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import "./styles/App.css";
+import Contacts from "./components/contacts/Contacts";
 
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+
+    <Router>
+      <div className="App">
+
+        <Navbar />
+        <div className="container">
+          <div className="py-3">
+            <Contacts/>
+          </div>
+        </div>
+        <Switch>
+
+        </Switch>
+
+      </div>
+
+    </Router>
   );
 }
 
