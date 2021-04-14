0fcce7254ca6b791289fbf51520b4bbd16deb64a
akash elhance <akashelhance@gmail.com>
2021-04-14 13:22:48 +0530

    redux store added

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import Navbar from "./components/elements/Navbar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { povider, Provider } from "react-redux";
+import store from "./store";
 
 import "./styles/App.css";
 import Contacts from "./components/contacts/Contacts";
 
 
+
 function App() {
   return (
+    <Provider store={store}>
+      <Router>
+        <div className="App">
 
-    <Router>
-      <div className="App">
-
-        <Navbar />
-        <div className="container">
-          <div className="py-3">
-            <Contacts/>
+          <Navbar />
+          <div className="container">
+            <div className="py-3">
+              <Contacts />
+            </div>
           </div>
+          <Switch>
+
+          </Switch>
+
         </div>
-        <Switch>
 
-        </Switch>
+      </Router>
+    </Provider>
 
-      </div>
-
-    </Router>
   );
 }
 
